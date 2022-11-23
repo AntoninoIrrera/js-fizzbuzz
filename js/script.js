c2ee5a926d9f0c9207e3f07c949b902cff1726ca
AntoninoIrrera <irrera.antonio2003@gmail.com>
2022-11-23 15:57:39 +0100

    Bonus 2 inserimento stili / colori a seconda del risultato ottenuto

--- js/script.js
+++ js/script.js
@@ -15,25 +15,25 @@ for (let i = 1; i <= 100; i++){
     
     if((i % 3 == 0) && (i % 5 == 0)){
         
-        const elemento = `<li class="mt-3"> FizzBuzz </li> `;
+        const elemento = `<li class="mt-3 rosso"> FizzBuzz </li> `;
         ul.innerHTML += elemento;
         // ul.append(li, "FizzBuzz");
         console.log("FizzBuzz");
     }
     else if(i % 3 == 0){
-        const elemento = `<li class="mt-3"> Fizz </li> `;
+        const elemento = `<li class="mt-3 magenta"> Fizz </li> `;
         ul.innerHTML += elemento;
         // ul.append(li, "Fizz");
         console.log("Fizz");
     }
     else if(i % 5 == 0){
-        const elemento = `<li class="mt-3"> Buzz </li> `;
+        const elemento = `<li class="mt-3 blu"> Buzz </li> `;
         ul.innerHTML += elemento;
         // ul.append(li, "Buzz");
         console.log("Buzz");
     }
     else{
-        const elemento = `<li class="mt-3"> ${i} </li> `;
+        const elemento = `<li class="mt-3 verde"> ${i} </li> `;
         ul.innerHTML += elemento;
         // ul.append(li, i);
         console.log(i);
