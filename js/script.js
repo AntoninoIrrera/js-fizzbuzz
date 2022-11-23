77d79ba1f2eb4a615b65d8d81db74cf18bee78b2
AntoninoIrrera <irrera.antonio2003@gmail.com>
2022-11-23 15:54:58 +0100

    Bonus 1, inserimento contenuto all'interno del DOM

--- js/script.js
+++ js/script.js
@@ -1,20 +1,41 @@
 
 
 
+const ul = document.querySelector("ul");
+    
+    
 for (let i = 1; i <= 100; i++){
+    
+    // const elemento = `<li> </li> `;
+    // ul.innerHTML += elemento;
 
+    // const li = document.querySelector("li");
 
+    // let li = document.createElement("li")
+    
     if((i % 3 == 0) && (i % 5 == 0)){
-        console.log("FizzBuzz")
+        
+        const elemento = `<li class="mt-3"> FizzBuzz </li> `;
+        ul.innerHTML += elemento;
+        // ul.append(li, "FizzBuzz");
+        console.log("FizzBuzz");
     }
     else if(i % 3 == 0){
-        console.log("Fizz")
+        const elemento = `<li class="mt-3"> Fizz </li> `;
+        ul.innerHTML += elemento;
+        // ul.append(li, "Fizz");
+        console.log("Fizz");
     }
     else if(i % 5 == 0){
-        console.log("Buzz")
+        const elemento = `<li class="mt-3"> Buzz </li> `;
+        ul.innerHTML += elemento;
+        // ul.append(li, "Buzz");
+        console.log("Buzz");
     }
     else{
-
+        const elemento = `<li class="mt-3"> ${i} </li> `;
+        ul.innerHTML += elemento;
+        // ul.append(li, i);
         console.log(i);
     }
 
