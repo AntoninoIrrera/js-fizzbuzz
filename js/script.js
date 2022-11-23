8c914deeb368f0a55cd949e50cc4994314e6678d
AntoninoIrrera <irrera.antonio2003@gmail.com>
2022-11-23 16:17:55 +0100

    ritorno metodo iniziale commit finale

--- js/script.js
+++ js/script.js
@@ -2,72 +2,63 @@
 const bottone = document.querySelector("a.btn-danger");
 const ul = document.querySelector("ul");
 
-/*  
-    bottone.addEventListener("click", function(){
-        for (let i = 1; i <= 100; i++) {
-            // const elemento = `<li> </li> `;
-            // ul.innerHTML += elemento;
-            // const li = document.querySelector("li");
-            // let li = document.createElement("li")
-            if ((i % 3 == 0) && (i % 5 == 0)) {
-                const elemento = `<li class="mt-3 rosso"> FizzBuzz </li> `;
-                ul.innerHTML += elemento;
-                // ul.append(li, "FizzBuzz");
-                console.log("FizzBuzz");
-            }
-            else if (i % 3 == 0) {
-                const elemento = `<li class="mt-3 magenta"> Fizz </li> `;
-                ul.innerHTML += elemento;
-                // ul.append(li, "Fizz");
-                console.log("Fizz");
-            }
-            else if (i % 5 == 0) {
-                const elemento = `<li class="mt-3 blu"> Buzz </li> `;
-                ul.innerHTML += elemento;
-                // ul.append(li, "Buzz");
-                console.log("Buzz");
-            }       else {
-                const elemento = `<li class="mt-3 verde"> ${i} </li> `;
-                ul.innerHTML += elemento;
-                // ul.append(li, i);
-                console.log(i);
-            }
-        } 
-    })
-*/     
 
-const p = document.querySelector("p");
-
-
-bottone.addEventListener("click", function () {
+bottone.addEventListener("click", function(){
     for (let i = 1; i <= 100; i++) {
-        // const elemento = `<li> </li> `;
-        // ul.innerHTML += elemento;
-        // const li = document.querySelector("li");
-        let br = document.createElement("br")
+        
         if ((i % 3 == 0) && (i % 5 == 0)) {
-            // const elemento = `<li class="mt-3 rosso"> FizzBuzz </li> `;
-            // ul.innerHTML += elemento;
-            p.append(br, "FizzBuzz");
+            const elemento = `<li class="mt-3 rosso"> FizzBuzz </li> `;
+            ul.innerHTML += elemento;
             console.log("FizzBuzz");
         }
         else if (i % 3 == 0) {
-            // const elemento = `<li class="mt-3 magenta"> Fizz </li> `;
-            // ul.innerHTML += elemento;
-            p.append(br, "Fizz");
+            const elemento = `<li class="mt-3 magenta"> Fizz </li> `;
+            ul.innerHTML += elemento;
             console.log("Fizz");
         }
         else if (i % 5 == 0) {
-            // const elemento = `<li class="mt-3 blu"> Buzz </li> `;
-            // ul.innerHTML += elemento;
-            p.append(br, "Buzz");
+            const elemento = `<li class="mt-3 blu"> Buzz </li> `;
+            ul.innerHTML += elemento;
             console.log("Buzz");
-        }
-        else {
-            // const elemento = `<li class="mt-3 verde"> ${i} </li> `;
-            // ul.innerHTML += elemento;
-            p.append(br, i);
+        }       else {
+            const elemento = `<li class="mt-3 verde"> ${i} </li> `;
+            ul.innerHTML += elemento;
             console.log(i);
         }
-    }
+    } 
 })
+   
+// -----------------------------
+
+
+// metodo con funzione append
+// const bottone = document.querySelector("a.btn-danger");
+// const p = document.querySelector("p");
+
+
+// bottone.addEventListener("click", function () {
+//     for (let i = 1; i <= 100; i++) {
+       
+//         let br = document.createElement("br")
+//         if ((i % 3 == 0) && (i % 5 == 0)) {
+            
+//             p.append(br, "FizzBuzz");
+//             console.log("FizzBuzz");
+//         }
+//         else if (i % 3 == 0) {
+            
+//             p.append(br, "Fizz");
+//             console.log("Fizz");
+//         }
+//         else if (i % 5 == 0) {
+            
+//             p.append(br, "Buzz");
+//             console.log("Buzz");
+//         }
+//         else {
+       
+//             p.append(br, i);
+//             console.log(i);
+//         }
+//     }
+// })
